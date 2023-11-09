a58fb93544e7df8d03ad5d2390b03fc6a04cc0e5
roadste <4b0g0124@stust.edu.tw>
2023-11-09 12:02:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 print('test')
+#1
 def function(name,age):
     print(name,age)
 
 function("violet",23)
 
+#2
 def func1(*args):
     for i in args:
         print(i)
@@ -11,6 +13,7 @@ def func1(*args):
 func1(20,40,60)
 func1(80,100)
 
+#3
 def calculate(a,b):
     add = a+b
     red = a-b
@@ -18,12 +21,14 @@ def calculate(a,b):
 res = calculate(40,10)
 print(res)
 
+#4
 def show_employee(name, salary=9000):
     print("name:",name, "salary:",salary)
 
 show_employee("Ben",12000)
 show_employee("tom")
 
+#5
 def outer_fun(a, b):
     square = a ** 2
 
@@ -39,6 +44,7 @@ def outer_fun(a, b):
 result = outer_fun(5, 10)
 print(result)
 
+#6
 def addition(num):
     if num:
         # call same function by reducing number by 1
@@ -49,6 +55,7 @@ def addition(num):
 res = addition(10)
 print(res)
 
+#7
 def display_student(name, age):
     print(name, age)
 
@@ -60,7 +67,9 @@ showStudent = display_student
 # call using new name
 showStudent("Emma", 26)
 
+#8
 print(list(range(4,30,2)))
 
+#9
 x= [4,6,8,24,12,2]
 print(max(x))
