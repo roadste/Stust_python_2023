2170132b761ef8ea5e46907d6c0d9d4f4d688dcb
roadste <4b0g0124@stust.edu.tw>
2023-11-09 11:36:05 +0800

    Updated test.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
 print('test')
+def function(name,age):
+    print(name,age)
+
+function("violet",23)
+
+def func1(*args):
+    for i in args:
+        print(i)
+
+func1(20,40,60)
+func1(80,100)
+
+def calculate(a,b):
+    add = a+b
+    red = a-b
+    return add,red
+res = calculate(40,10)
+print(res)
+
+
