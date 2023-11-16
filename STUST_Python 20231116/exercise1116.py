d8bee08314d9b1e349eba675c9de369dea3266a6
roadste <112836972+roadste@users.noreply.github.com>
2023-11-16 11:57:29 +0800

    Update exercise1116.py

--- STUST_Python 20231116/exercise1116.py
+++ STUST_Python 20231116/exercise1116.py
@@ -19,3 +19,25 @@ print(p1.age)
 print(p1)
 print(p2)
 p1.myfunc()
+
+'參數 邊 長 寬 半徑'
+'此類別用來求面積'
+class Myshape:
+    def __init__ (self, side, length, width,radius):
+        self.side = side
+        self.length = length
+        self.width = width
+        self.radius = radius
+    
+    def rectangle(self):
+        print("長方面積 = ",self.length*self.width)
+    def square(self):
+        print("正方面積 = ",self.side*self.side)
+    def round(self):
+        print("圓面積 = ",pow(self.radius,2)*3.14)
+
+p1 = Myshape(2,2,3,2)
+
+p1.rectangle()
+p1.square()
+p1.round()
