9bd29ca9435077c0e86d4865b4b4e88dd60fa19f
roadste <112836972+roadste@users.noreply.github.com>
2023-11-23 11:12:23 +0800

    Update exercise1123.py

---
+++ STUST_Python_20231123/exercise1123.py
@@ -0,0 +1,49 @@
+class Employee:
+    def __init__(self, emp_id, emp_name, emp_salary, emp_department, emp_hour):
+        self.emp_id = emp_id
+        self.emp_name = emp_name
+        self.emp_salary = emp_salary
+        self.emp_department = emp_department
+        self.emp_hour = emp_hour
+
+    def calculate_emp_salary(self):
+        overhour=0
+        overpay = 0
+        pay = 0
+
+        if self.emp_hour > 50:
+             overhour= self.emp_hour - 50
+             overpay = 50 * self.emp_salary + (overhour * (self.emp_salary/50))
+             print(overpay)
+
+        else :
+            pay = self.emp_salary * self.emp_hour
+            print(pay)
+
+        #print(self.emp_salary)
+
+    def emp_assign_department(self):
+        print(self.emp_department)
+
+    def print_employee_details(self):
+        print(self.emp_id,self.emp_name)
+
+e1 = Employee ("E7876","亞當斯",50000,"會計",40)
+e1.print_employee_details()
+e1.emp_assign_department()
+e1.calculate_emp_salary()
+
+e2 = Employee ("E7499","瓊斯",45000,"研究",60)
+e2.print_employee_details()
+e2.emp_assign_department()
+e2.calculate_emp_salary()
+
+e3 = Employee ("E7900","馬丁",50000,"銷售",50)
+e3.print_employee_details()
+e3.emp_assign_department()
+e3.calculate_emp_salary()
+
+e4 = Employee ("E7698","史密斯",55000,"操作",58)
+e4.print_employee_details()
+e4.emp_assign_department()
+e4.calculate_emp_salary()
